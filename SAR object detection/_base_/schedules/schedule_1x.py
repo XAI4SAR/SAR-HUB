027ce79df4526422c29fb496bbad4a35cab5c645
Caaaaaaaaat <93436865+Newinthus@users.noreply.github.com>
2023-06-11 15:16:04 +0800

    Add files via upload

---
+++ SAR object detection/_base_/schedules/schedule_1x.py
@@ -0,0 +1,11 @@
+# optimizer
+optimizer = dict(type='SGD', momentum = 0.9, lr = 0.005, weight_decay = 0.001)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[24, 33])
+total_epochs = 36
